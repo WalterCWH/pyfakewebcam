22d6d73960b6bd4a61c9353a5be4af3358d25abe
weiheng.cheng <weiheng.cheng@starfivetech.com>
2024-01-19 13:42:12 +0800

    pyfakewebcam.py: Add support to stream JPEG pixel format Set JPEG as default pixel format to be streamed.
    Signed-off-by: weiheng.cheng <weiheng.cheng@starfivetech.com>

--- pyfakewebcam/pyfakewebcam.py
+++ pyfakewebcam/pyfakewebcam.py
@@ -19,7 +19,7 @@ class FakeWebcam:
     
     # TODO: add support for more pixfmts
     # TODO: add support for grayscale
-    def __init__(self, video_device, width, height, channels=3, input_pixfmt='RGB'):
+    def __init__(self, video_device, width, height, channels=3, input_pixfmt='RGB', output_pixfmt=_v4l2.V4L2_PIX_FMT_JPEG):
         
         if channels != 3:
             raise NotImplementedError('Code only supports inputs with 3 channels right now. You tried to intialize with {} channels'.format(channels))
@@ -37,7 +37,7 @@ class FakeWebcam:
                     
         self._settings = _v4l2.v4l2_format()
         self._settings.type = _v4l2.V4L2_BUF_TYPE_VIDEO_OUTPUT
-        self._settings.fmt.pix.pixelformat = _v4l2.V4L2_PIX_FMT_YUYV
+        self._settings.fmt.pix.pixelformat = output_pixfmt
         self._settings.fmt.pix.width = width
         self._settings.fmt.pix.height = height
         self._settings.fmt.pix.field = _v4l2.V4L2_FIELD_NONE
@@ -98,3 +98,14 @@ class FakeWebcam:
             
         os.write(self._video_device, self._buffer.tostring())
 
+    # Expect image input from cv2.imread() function and send encoded jpeg buffer to v4l2loopback dummy device
+    def schedule_frame_jpeg(self, frame):
+        if frame.shape[0] != self._settings.fmt.pix.height:
+            raise Exception('frame height does not match the height of webcam device: {}!={}\n'.format(self._settings.fmt.pix.height, frame.shape[0]))
+        if frame.shape[1] != self._settings.fmt.pix.width:
+            raise Exception('frame width does not match the width of webcam device: {}!={}\n'.format(self._settings.fmt.pix.width, frame.shape[1]))
+        if frame.shape[2] != self._channels:
+            raise Exception('num frame channels does not match the num channels of webcam device: {}!={}\n'.format(self._channels, frame.shape[2]))
+
+        buffer_string = cv2.imencode('.jpg', frame)[1]
+        os.write(self._video_device, buffer_string)
